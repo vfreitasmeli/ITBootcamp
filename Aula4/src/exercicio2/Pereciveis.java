87732b7c81cb7928b818e56d47b0887702357970
Viviane De Freitas <viviane.dfreitas@mercadolivre.com>
2022-06-28 18:47:29 -0300

    Exercicio 2 - Parte B (em testes)

--- Aula4/src/exercicio2/Pereciveis.java
+++ Aula4/src/exercicio2/Pereciveis.java
@@ -1,2 +1,48 @@
-package exercicio2;public class Pereciveis {
+package exercicio2;
+
+public class Pereciveis extends Produtos {
+    int diasParaVencer;
+
+    public Pereciveis(String nome, double preco) {
+        super(nome, preco);
+    }
+
+    public int getDiasParaVencer() {
+        return diasParaVencer;
+    }
+
+    public void setDiasParaVencer(int diasParaVencer) {
+        this.diasParaVencer = diasParaVencer;
+    }
+
+    @Override
+    public String toString() {
+        return "Pereciveis{" +
+                "diasParaVencer=" + diasParaVencer +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+
+    @Override
+    public double calcular(int quantidadeDeProdutos) {
+
+        switch (diasParaVencer) {
+            case 1:
+                resultado = super.calcular( quantidadeDeProdutos )/ 4;
+                break;
+            case 2:
+                resultado = super.calcular(quantidadeDeProdutos) / 3;
+                break;
+            case 3:
+                resultado = super.calcular(quantidadeDeProdutos) / 2;
+                break;
+            default:
+                resultado = super.calcular(quantidadeDeProdutos);
+                break;
+        }
+
+        System.out.println(resultado);
+        return resultado;
+    }
 }
