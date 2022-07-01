2ae729a6fb74f4d23764df7418fe2560cb117889
Viviane De Freitas <viviane.dfreitas@mercadolivre.com>
2022-07-01 12:42:31 -0300

    Exercicio 3 - pratica completa

--- Aula5/src/exercicio3/SonsApp.java
+++ Aula5/src/exercicio3/SonsApp.java
@@ -2,18 +2,26 @@ package exercicio3;
 
 public class SonsApp {
     public static void main(String[] args) {
-        Animal a1 = new Cachorro("Doguinho", "au au");
-        Animal a2 = new Gato("Gatíneo", "miau");
-        Vaca v3 = new Vaca("Mimosa", "muuuu"); // forma diferente de instanciar chamando a filha func igual
+        Cachorro cachorro = new Cachorro("Doguinho", "au au");
+        Gato gato = new Gato("Gatíneo", "miau");
+        Vaca vaca = new Vaca("Mimosa", "muuuu");
 
-        a1.emitirSom();
-        a2.emitirSom();
-        v3.emitirSom();
+        cachorro.emitirSom();
+        cachorro.comerCarne();
+
+        gato.emitirSom();
+        gato.comerCarne();
+
+        vaca.emitirSom();
+        vaca.comerPasto();
     }
 
 
     /*
     anotacao pessoal: o som e nome sao comuns a todos os animais,
     logo fica declarado na classe abstrata junto com a funcao emitirSom, tambem comum a todos
+
+    com a interface implementada eu nao consigo instanciar diretamente pela classe abstrata, preciso chamar diretamente
+    pela classe filha onde a interface está.
      */
 }
